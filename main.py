22cd9c46e81af7e74c1e7043e42d8452f645c019
santiago1401barr <127438958+santiago1401barr@users.noreply.github.com>
2024-02-10 21:16:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ with Datos_Financieros:
                                                                      'balance-sheet-statement-growth','cash-flow-statement-growth','ratios-ttm','ratios','financial-growth',
                                                                     'quote','rating','enterprise-values','key-metrics-ttm','key-metrics','historical-rating','discounted-cash-flow',
                                                                      'historical-discounted-cash-flow-statement')
-    url = f'{base_url}/{datos_financieros_extras}/{ticker}?apikey={API_KEY_FMP}'
+    url = f'{url_base}/{datos_financieros_extras}/{ticker}?apikey={API_KEY_FMP}'
     df_financiero = requests.get(url).json()
     st.write(df)
 
